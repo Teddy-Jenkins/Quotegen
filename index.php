acb53500cae1016ac31306a71c63a1ed3d84bb13
Charles Langley <cianlangley@hotmail.com>
2018-10-14 20:26:25 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Quote Gen</h1>
+    <button type="button" name="button">Press To Begin</button>
+
+  </body>
+</html>
---
+++ quote.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="" value="Title">
+      <input type="text" name="" value="Client Name">
+      <input type="text" name="" value="Item">
+      <input type="text" name="" value="Total Price">
+    </form>
+  </body>
+</html>
